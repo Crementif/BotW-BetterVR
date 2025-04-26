3698259fbf3fb95204d9760940568a8e2419ab08
Crementif <26669564+Crementif@users.noreply.github.com>
2025-04-26 21:23:24 +0200

    Remove all legacy patches and code

--- resources/BreathOfTheWild_BetterVR/patch_UpdateProjectionMatrix.asm
+++ resources/BreathOfTheWild_BetterVR/patch_UpdateProjectionMatrix.asm
@@ -4,55 +4,6 @@ moduleMatches = 0x6267BFD0
 .origin = codecave
 
 
-ASPECT_STACK_OFFSET = 0x04
-FOVY_STACK_OFFSET = 0x08
-OFFSET_X_STACK_OFFSET = 0x0C
-OFFSET_Y_STACK_OFFSET = 0x10
-
-
-updateCameraOffset:
-mflr r0
-stwu r1, -0x18(r1)
-stw r0, 0x1C(r1)
-
-addi r11, r1, 0x04
-bl import.coreinit.hook_UpdateCameraOffset
-
-lwz r11, 0xCD4(r29)
-lfs f10, FOVY_STACK_OFFSET(r1)
-stfs f10, 0x68(r11)
-lfs f9, OFFSET_X_STACK_OFFSET(r1)
-stfs f9, 0x6C(r11)
-lfs f10, OFFSET_Y_STACK_OFFSET(r1)
-stfs f10, 0x70(r11)
-
-lwz r0, 0x1C(r1)
-mtlr r0
-addi r1, r1, 0x18
-blr
-
-;0x02C07708 = bla updateCameraOffset
-
-
-updateCameraAspectRatio:
-mflr r0
-stwu r1, -0x10(r1)
-stw r0, 0x14(r1)
-
-addi r28, r1, 0x04
-bl import.coreinit.hook_CalculateCameraAspectRatio
-
-lfs f12, ASPECT_STACK_OFFSET(r1)
-stfs f12, 0xB4(r30)
-
-lwz r0, 0x14(r1)
-mtlr r0
-addi r1, r1, 0x10
-blr
-
-;0x0386D024 = bla updateCameraAspectRatio
-
-
 loadLineFormat:
 .string "sead::PerspectiveProjection::setFovY(this = 0x%08x fovRadians = %f offsetX = %f near = %f far = %f) LR = 0x%08x %c"
 printSetFovY:
